c56c62fa6386b0170e356c54a32468a6efb7da8c
artemyakovenko <jeep2801@gmail.com>
2018-12-12 20:59:39 +0200

    remove 'updated' and 'created' setting on frontend, fix dateTime pattern and dump.sql

--- src/main/java/com/studysoft/smartjournal/service/util/Constants.java
+++ src/main/java/com/studysoft/smartjournal/service/util/Constants.java
@@ -2,7 +2,7 @@ package com.studysoft.smartjournal.service.util;
 
 public final class Constants {
 
-    public static final String DATE_TIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'";
+    public static final String DATE_TIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
     public static final String EASTERN_EUROPEAN_OFFSET = "+2";
 
     private Constants() {}
