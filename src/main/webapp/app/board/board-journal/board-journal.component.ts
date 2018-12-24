1cbe819f07d5877bbb611dbaf1edde8298d0afd3
artemyakovenko <jeep2801@gmail.com>
2018-12-24 15:58:27 +0200

    separate BoardDetailComponent to BoardJournalComponent and BoardTasksComponent

--- src/main/webapp/app/board/board-journal/board-journal.component.ts
+++ src/main/webapp/app/board/board-journal/board-journal.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Group } from 'app/shared/model/group.model';
 
 @Component({
-  selector: 'jhi-board-journal',
-  templateUrl: './board-journal.component.html',
-  styles: []
+    selector: 'jhi-board-journal',
+    templateUrl: './board-journal.component.html',
+    styles: []
 })
 export class BoardJournalComponent implements OnInit {
+    @Input() group: Group;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
--- src/main/webapp/app/board/board-tasks/board-tasks.component.ts
+++ src/main/webapp/app/board/board-tasks/board-tasks.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Board } from 'app/shared/model/board.model';
 
 @Component({
-  selector: 'jhi-board-tasks',
-  templateUrl: './board-tasks.component.html',
-  styles: []
+    selector: 'jhi-board-tasks',
+    templateUrl: './board-tasks.component.html',
+    styles: []
 })
 export class BoardTasksComponent implements OnInit {
+    @Input() board: Board;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
